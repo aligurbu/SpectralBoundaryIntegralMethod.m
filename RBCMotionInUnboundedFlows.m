2acfcb10f24ada2b11c53b7f261610e8ac4a27aa
aligurbu <aligurbu@buffalo.edu>
2022-12-20 17:56:38 -0800

    Create RBCMotionInUnboundedFlows.m

---
+++ RBCMotionInUnboundedFlows.m
@@ -0,0 +1,5 @@
+%% Spectral Galerkin BIE implementation for an RBC motion and deformation
+%% in an unbounded domain under the ambient flow field
+%%
+clear all; close all; clc;
+addpath(genpath('../SpectralBoundaryIntegralMethod.m'))
