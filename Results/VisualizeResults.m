3095007a03d8092557f6d5096385cc105ab1ba32
aligurbu <aligurbu@buffalo.edu>
2022-12-20 23:21:00 -0800

    Create VisualizeResults.m

---
+++ Results/VisualizeResults.m
@@ -0,0 +1,34 @@
+%% Visualize the results
+clear all; close all; clc;
+addpath(genpath('../../SpectralBoundaryIntegralMethod.m'))
+verbose_Plot = false;
+WritetoGIF = false; % if false then save as MPEG-4
+
+%% Input the model and parameters for the analysis from Models folder
+LoadElasticRBC_Shear_N16
+% LoadElasticRBC_Parabolic_N16
+% LoadMemViscosityRBC_Shear_N16
+% LoadMemViscosityRBC_Parabolic_N16
+
+%% Set up output file
+fidCoord = fopen(['Coord_',name,'.dat'],'r');
+fidMemFor = fopen(['MemFor_',name,'.dat'],'r');
+fidSol = fopen(['Sol_',name,'.dat'],'r');
+fidTime = fopen(['Time_',name,'.dat'],'r');
+Time = fread(fidTime,'double');
+NSTEPS = length(Time);
+
+%%
+timeStepIncrement = 1;
+Rendering = 2; % This upsampling for the rendering purposes
+
+%% Choose a view angle
+viewInd = [0 90];
+
+%% Choose a background color for visualization
+blackBackground = true; % if false then white background
+
+%% Run Visualize m-files one at a time.
+VisualizeMembraneShape
+
+fclose('all');
