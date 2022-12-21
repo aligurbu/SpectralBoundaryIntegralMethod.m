ab667c98401f32c2d6ac8071cfa629d1af980962
aligurbu <aligurbu@buffalo.edu>
2022-12-20 17:34:30 -0800

    Fluid properties

--- Models/LoadElasticRBC_Shear_N16.m
+++ Models/LoadElasticRBC_Shear_N16.m
@@ -4,3 +4,12 @@ name = 'ElasticRBC_Shear_N16';
 
 %% Reference parameters 
 ReferenceParameters
+
+%% Parameters 
+%% Fluid properties
+%% Viscosity of exterior fluid
+mu_out = 1.2*10^(-3); % Pa.s 
+mu = mu_out/RefViscosity; 
+%% Viscosity of interior fluid
+mu_in = 6*10^(-3); % Pa.s
+lam = mu_in/RefViscosity; % lam*mu
