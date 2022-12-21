5d5ceac9f2c13b97ec0cfbca22fe7e944a6196fa
aligurbu <aligurbu@buffalo.edu>
2022-12-20 21:07:27 -0800

    Update AnalyzeResults.m

--- Results/AnalyzeResults.m
+++ Results/AnalyzeResults.m
@@ -1,6 +1,7 @@
 %% Analyze the results
 clear all; close all; clc;
 addpath(genpath('../../SpectralBoundaryIntegralMethod.m'))
+verbose_Plot = true;
 
 %% Input the model and parameters for the analysis from Models folder
 LoadElasticRBC_Shear_N16
@@ -17,7 +18,14 @@ Time = fread(fidTime,'double');
 NSTEPS = length(Time);
 
 %%
-timeStepIncrement = 10;
+timeStepIncrement = 1;
 
 %% Analyze the results of RBCMotionInUnboundedFlows
 AnalyzeResults_RBCMotion
+
+%% Plots
+%% Visualization settings
+VisualizeSettings
+
+%% Forces and moments balance errors
+Force_Moment_BalanceError
